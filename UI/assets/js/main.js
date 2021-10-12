6e4984b2b5588de3ed75db7db2c41e476c78e565
SuongNguyenThanh <ntsuong0309@gmail.com>
2021-10-12 23:04:08 +0700

    creat foder assets > css,img,js and create file main.js, style.css

---
+++ UI/assets/js/main.js
@@ -0,0 +1,34 @@
+
+  var theme = {
+    strings: {
+      select_options: "Select Options",
+      addToCart: "Add to cart",
+      soldOut: "Sold out",
+      unavailable: "Unavailable",
+      showMore: "Show More",
+      showLess: "Show Less",
+      addressError: "Error looking up that address",
+      addressNoResults: "No results for that address",
+      addressQueryLimit: "You have exceeded the Google API usage limit. Consider upgrading to a \u003ca href=\"https:\/\/developers.google.com\/maps\/premium\/usage-limits\"\u003ePremium Plan\u003c\/a\u003e.",
+      authError: "There was a problem authenticating your Google Maps account.",
+      total: "Total",
+      spend: "Spend",
+      content_threshold: "Congratulations! You\u0026#39;ve got free shipping!",
+      spend__html: "for free shipping",
+      check_out: "Check out",
+      remove: "Remove",
+      remove_wishlist: "Remove Wishlist",
+      add_to_wishlist: "translation missing: en.wishlist.wishlist.add_to_wishlist",
+      added_to_wishlist: "translation missing: en.wishlist.wishlist.added_to_wishlist",
+      view_cart: "View Cart"
+    },
+    moneyFormat: "\u003cspan class=\"money\"\u003e${{amount}}\u003c\/span\u003e",
+    moneyFormatnojson: "\u003cspan class=\"money\"\u003e${{amount}}\u003c\/span\u003e",
+    freeshipping_value: 2000
+  }
+  window.ajaxcart_type = 'pop_up';
+  window.file_url = '//cdn.shopify.com/s/files/1/0272/1493/8165/files/?37953';
+  window.asset_url = '//cdn.shopify.com/s/files/1/0272/1493/8165/t/30/assets/?37953';
+  window.sidebar_multichoise = true;
+  document.documentElement.className = document.documentElement.className.replace('no-js', 'js');
+  window.router = "";
